bb818b4779b5ae2db65dffbd4d69ff6688e45495
Emre Sumer <emre@Emres-MacBook-Pro.local>
2018-01-24 18:38:21 +0300

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Emre Sumer on 24/01/2018.
+//  Copyright © 2018 Emre Sumer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
